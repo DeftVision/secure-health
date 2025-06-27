e8e773751aefc6bc1ecbd405f4e1edd99ef460a5
Gabriel <gjbarret@asu.edu>
2025-06-26 20:13:10 -0600

    Install and configuring Knex

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const db = require('./config/db');
 
 const express = require('express');
 const cors = require('cors');
@@ -18,6 +19,17 @@ app.use(morgan('dev'));        // Logs HTTP requests (dev format)
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Secure Health API is running.' });
 });
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await db.query('SELECT NOW()');
+        res.status(200).json({ message: 'Secure Health API is running.', time: result.rows[0].now });
+    } catch (err) {
+        console.error('Database error:', err);
+        res.status(500).json({ error: 'Database connection failed' });
+    }
+});
+
 
 // Start server
 app.listen(port, () => {
