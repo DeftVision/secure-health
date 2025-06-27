c4fe39b50c4cd93783281bd38bca74be45ca5176
Gabriel <gjbarret@asu.edu>
2025-06-26 23:12:26 -0600

    Adds authentication and authorization
    Implements authentication middleware using JWT for verifying user identity.
    Introduces role-based authorization to control access to protected resources.
    Adds a protected route to demonstrate authentication and authorization.

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 const port = process.env.PORT || 5001;
+const protectedRoutes = require('./routes/protected.routes');
 
 // Security & parsing middleware
 app.use(helmet());             // Sets secure HTTP headers
@@ -16,6 +17,7 @@ app.use(cors());               // Allows cross-origin requests
 app.use(express.json());       // Parses incoming JSON
 app.use(morgan('dev'));        // Logs HTTP requests (dev format)
 app.use('/auth', authRoutes);
+app.use('/protected', protectedRoutes);
 
 
 // Base route
